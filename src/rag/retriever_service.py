f60a7ecebf8b971f8eaf58d24974afa9a3c23a1f
Shashank Srivatsav Vaddiparthi <shashanksrivatsav123@gmail.com>
2025-10-30 20:22:32 +0530

    Update retrieval code

--- src/rag/retriever_service.py
+++ src/rag/retriever_service.py
@@ -3,57 +3,47 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain.chat_models import init_chat_model
 # from langchain.chains import create_retrieval_chain
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 # from langchain_core.output_parsers import StrOutputParser
 from src.vectorstore.vector_service import load_vectorstore
 
 load_dotenv()
 
-def build_qa_prompt():
+def build_qa_prompt(context: str, query: str):
     """Builds RAG prompt using FAISS and Gemini 2.5 Flash."""
-    # vectorstore = load_vectorstore("iaka_index")
-
-    # llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
-    
-    # retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
-
-    template = """
-    You are an internal AI Knowledge Assistant. 
-    Use the provided context to answer the question concisely. 
-    If you cannot find the answer, say "Answer not found in knowledge base."
-    
-    Context: 
-    {context} 
-
-    Questions:
-    {question}
-    """
-
-    prompt = PromptTemplate(
-        input_variables=["context", "question"], 
-        template=template
+    prompt_template = ChatPromptTemplate(
+        messages=[
+            (
+                "system", 
+                "You are an internal AI Knowledge Assistant. "
+                "Use the provided context to answer the question concisely. "
+                "If you cannot find the answer, say 'Answer not found in knowledge base.'\"\n\n"
+                "Context: {context} \n\n"
+            ),
+            (
+                "user", 
+                "Query: {query}"
+            )
+        ]
     )
-
-    # chain = RetrievalQA.from_chain_type(
-    #     llm=llm, 
-    #     retriever=retriever,
-    #     return_source_documents=True, 
-    #     chain_type_kwargs={"prompt": prompt}
-    # )
-
+    prompt = prompt_template.invoke({"context": context, "query": query})
     return prompt
 
 def query_knowledge_base(query: str):
     """Query documents using Gemini RAG pipeline."""
     vectorstore = load_vectorstore("iaka_index")
-    llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
     retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+    
+    llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+    
+    docs = retriever.invoke(query)
+    context = "\n\n".join(doc.page_content for doc in docs)
 
-    prompt_template = build_qa_prompt()
-    result = prompt_template.invoke({"context": retriever.invoke(query), "question": query})
-    response = llm.invoke(result)
+    prompt = build_qa_prompt(context, query)
+    response = llm.invoke(prompt)
+    
     return {
         "answer": response.content, 
-        "prompt": result
-        # "sources": [doc.metadata for doc in result["source_documents"]]
+        "prompt": prompt, 
+        "sources": [doc.metadata for doc in docs]
     }
